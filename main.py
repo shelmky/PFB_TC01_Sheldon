b6f9063ed13793fdbbc2ae1edaa00872cbd974c6
jojovivii <158539641+jojovivii@users.noreply.github.com>
2024-02-02 11:50:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from overheads import calculate_overheads
 from cash_on_hand import calculate_cash_on_hand
 from profit_loss import calculate_net_profit
-
 o = calculate_overheads()
 c = calculate_cash_on_hand()
 n =calculate_net_profit()
